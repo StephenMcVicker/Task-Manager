c2bd3f04f9f65ecd7a4e202710e07639dd23be5d
NeoDragon <mcvickerstephen@gmail.com>
2020-04-06 00:25:09 +0100

    Updates

---
+++ precache-manifest.facd0db8a02cde47fd27cee994f6cf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e77cfe386bd099b3c8253cb206459d",
+    "url": "/Task-Manager/index.html"
+  },
+  {
+    "revision": "b59542ab75f08668e011",
+    "url": "/Task-Manager/static/css/main.0d380032.chunk.css"
+  },
+  {
+    "revision": "444571609211c13fc99d",
+    "url": "/Task-Manager/static/js/2.24760f81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Task-Manager/static/js/2.24760f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59542ab75f08668e011",
+    "url": "/Task-Manager/static/js/main.b0511914.chunk.js"
+  },
+  {
+    "revision": "4043c93fa4d7ef34ab09",
+    "url": "/Task-Manager/static/js/runtime-main.731724e1.js"
+  }
+]);
